bfb2f4638d250491825830447659c0d3a50d3406
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2020-03-13 13:54:29 +0000

    First Commit
    Init off Spring framework.

---
+++ wa1/src/main/java/pt/unl/fct/csd/CsdProjectApplication.java
@@ -0,0 +1,13 @@
+package pt.unl.fct.csd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdProjectApplication.class, args);
+	}
+
+}
---
+++ wa1/src/test/java/pt/unl/fct/csd/CsdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.unl.fct.csd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
