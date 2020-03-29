628e663ba99ba3c43706bd7df1486b1c0f26de4f
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2020-03-29 09:40:13 +0100

    Remove server from client

--- wa1/Client/src/main/java/pt/unl/fct/csd/cliente/Cliente/ClienteApplication.java
+++ wa1/Client/src/main/java/pt/unl/fct/csd/cliente/Cliente/ClienteApplication.java
@@ -1,13 +1,17 @@
 package pt.unl.fct.csd.cliente.Cliente;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClienteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClienteApplication.class, args);
+		SpringApplication app = new SpringApplication(ClienteApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
+		//SpringApplication.run(ClienteApplication.class, args);
 	}
 
 }
