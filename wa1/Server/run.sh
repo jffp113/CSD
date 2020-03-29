28faef23f9cbc03dc06dcb8aafed8a4f7505e71b
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2020-03-29 13:55:44 +0100

    Supported Activated for MySQL

--- wa1/Server/run.sh
+++ wa1/Server/run.sh
@@ -11,10 +11,17 @@ docker network create \
   --gateway=172.1.0.1 csd || true
 
 
-for i in {1..1}
+for i in {1..4}
 do
-  #docker run --rm -d --network=csd -e MYSQL_ROOT_PASSWORD=toor --name "db${i}" csddatabase
-  docker run --rm -it --network=csd -p $((SERVER_PORT + i)):${SERVER_PORT}  -e MYSQL_HOST="db${i}" \
+  rm -rf "$(pwd)/Database/db${i}"
+  docker run --rm -d --network=csd -v "$(pwd)/Database/db${i}":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=toor --name "db${i}" csddatabase
+done
+
+sleep 15
+
+for i in {1..4}
+do
+  docker run --rm -d --network=csd -p $((SERVER_PORT + i)):${SERVER_PORT}  -e MYSQL_HOST="db${i}" \
    -e "REPLICA_ID=${REPLICA_ID}"  -e "SERVER_PORT=${SERVER_PORT}" --name "replica${i}" server
   ((REPLICA_ID++))
   ((END_IP++))
